2065bf1cf7079e6856b310c2af842516f665cc44
Skabunkel <skabunkel@users.noreply.github.com>
2020-10-28 22:22:17 +0100

    feat(core): Added finished basic core. * Added support for setting destination folder * Setup logging structure. * Tested with basic folder setup locally.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
-import signal
 import sys
+from os import path
 
 """ Only allow python3, and because it's python imp(importlib) was renamed after python 3.4 """
 if sys.version_info[0] < 3:
@@ -12,46 +12,20 @@ from modules import processing as proc
 import systemd.daemon as daemon
 import yaml
 
-log = log.get_journal_logger("backup")
+
 config_yaml = "task.yml"
 
-""" Define signal handlers for SIGTERM, and SIGHUP(reload) """
-def sigterm_handler(_signo, _stack):
-  daemon.notify('STOPPING=1')
-  proc.stop_processing()
-  sys.exit(0)
-
-
-def reload_handler(_signo, _stack):
-  daemon.notify('RELOADING=1')
-
-  #proc.stop_processing()
-  # reload ini file, then task.yml file.
-  #proc.start_processing()
-
-  daemon.notify('READY=1')
-
 if __name__ == '__main__':
-  signal.signal(signal.SIGTERM, sigterm_handler)
-  signal.signal(signal.SIGHUP, reload_handler) # SIGHUP is used as a reload signal for many daemons.
+  log = log.get_journal_logger("backup")
 
+  if not path.isfile(config_yaml):
+    log.info(f"unable to find config file:\"{config_yaml}\"")
+
+  with open(config_yaml) as file:
+    config = yaml.safe_load(file)
+  
+  log.info("Starting")
   daemon.notify('READY=1')
-
-  config = yaml.safe_load(""" 
-url: 'no'
-schedule: '*/5 * * * *'
-jobs:
- - name: 'Syncing system configs' # Syncs configs to default location.
-   schedule: '0 */24 * * *'
-   targets:
-    - '/srv/'
- 
- - name: 'Sync cheese stash'
-   url: 'no'
-   targets: 
-    - '/home/niklas'
-  """)
-
   proc.start_processing(config, log)
 
 
--- modules/processing.py
+++ modules/processing.py
@@ -2,6 +2,10 @@ from modules import backupJobs
 from datetime import datetime
 from queue import PriorityQueue
 import os
+import time
+import platform
+import urllib
+import subprocess
 
 """
 I'm still thinking about this file, i'll get back to you.
@@ -18,38 +22,65 @@ def start_processing(config, logger):
   if ('jobs' not in config) or (not isinstance(config['jobs'], list)):
     raise Exception("No jobs to perform.")
 
+  hostname = platform.node()
+
   url = 'localhost'
   schedule = config['schedule']
   date = datetime.now()
   user = os.environ["USER"]
+  to = '/tmp/'
   taskqueue = PriorityQueue()
 
   if ('user' in config) and isinstance(config['user'], str):
     user = config['user']
+
+  if ('to' in config) and isinstance(config['to'], str):
+    to = backupJobs.format_sync_destination(hostname, config['to'])
   
   if ('url' in config) and isinstance(config['url'], str):
     url = config['url']
 
   for job_conf in config['jobs']:
-    job = backupJobs.make_job(url, schedule, date, user, job_conf)
+    #make_job(hostname, default_url, default_schedule, current_date, default_user, default_to, job):
+    job = backupJobs.make_job(hostname, url, schedule, date, user, to, job_conf)
 
     if len(job.targets) != 0:
       taskqueue.put(job)
   
   config.clear()
 
-
-
-
-""" STUB WILL CHANGE """
-def stop_processing():
-  print("STUB!")
-
+  processor(taskqueue, logger)
 
 
 def processor(queue, logger):
-  print("STUB!")
-  # something like this
-  # rsync -a --delete from user@host:to
-  # -a is for archive.
-  # --delete sync file deletions to the target
+  if queue.empty():
+    logger.error("No valid tasks where found.")
+    return
+  
+  while True:
+    job = queue.get()
+    time_left = job.wait_to_run()
+    if time_left <= 0:
+      _sync(job, logger)
+    
+    queue.put(job)
+    if time_left > 0:
+      time.sleep(time_left)
+
+def _sync(job, logger):
+
+  user_host = f'{job.user}@{job.url}'
+  dest_root = job.destination
+  for target in job.targets:
+    source = target
+    dest = target.strip('/')
+    destination = f'{user_host}:{os.path.join(dest_root, dest)}'
+    logger.info(f'syncing from:\"{target}\" to:\"{destination}\"')
+    #subprocess.call(['rsync', '-a', '--delete', source, destination])
+    logger.info(' '.join(['rsync', '-a', '--delete', source, destination]))
+    # something like this
+    # rsync -a --delete from user@host:to
+    # -a is for archive.
+    # --delete sync file deletions to the target
+
+  logger.info(f'Job:\"{job.name}\" is done, rescheduled for:\"{job.moveto_next()}\"')
